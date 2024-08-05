fab43052563264b50349de6c639aec06601c26e2
cgtyunerr <cgtyuner06@gmail.com>
2024-08-06 02:03:50 +0300

    add error handler middleware

---
+++ app/middlewares/__init__.py
@@ -0,0 +1,4 @@
+"""App middleware package."""
+from .error_handler import GenericErrorHandlerMiddleware
+
+__all__ = ["GenericErrorHandlerMiddleware"]
---
+++ app/middlewares/error_handler.py
@@ -0,0 +1,37 @@
+"""Error handler middleware module."""
+from typing import Type
+
+from fastapi import status
+from fastapi.responses import ORJSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+
+from app.modules import (
+    ConflictError,
+    InvalidInputError,
+    NotFoundError,
+    UnprocessableEntityError,
+)
+
+custom_errors: dict[Type[Exception], int] = {
+    ConflictError: status.HTTP_409_CONFLICT,
+    InvalidInputError: status.HTTP_400_BAD_REQUEST,
+    NotFoundError: status.HTTP_404_NOT_FOUND,
+    UnprocessableEntityError: status.HTTP_422_UNPROCESSABLE_ENTITY,
+}
+
+
+class GenericErrorHandlerMiddleware(BaseHTTPMiddleware):
+    """Generic error handler middleware."""
+
+    async def dispatch(self, request, call_next):
+        """Dispatch the request."""
+        try:
+            response = await call_next(request)
+            return response
+        except Exception as e:
+            return ORJSONResponse(
+                status_code=custom_errors.get(
+                    type(e), status.HTTP_500_INTERNAL_SERVER_ERROR
+                ),
+                content={"detail": str(e)},
+            )
