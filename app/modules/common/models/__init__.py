9d24f71fb7754f723fa985c4f0aa75154a694b44
cgtyunerr <cgtyuner06@gmail.com>
2024-08-07 01:01:30 +0300

    add types models package

--- app/modules/common/models/__init__.py
+++ app/modules/common/models/__init__.py
@@ -1,8 +1,21 @@
 """Common models package."""
-from .mixin import IDMixin, TimeStampMixin, IsActiveMixin
+from .mixin import (
+    IDMixin,
+    TimeStampMixin,
+    IsActiveMixin,
+    integer,
+    real,
+    boolean,
+    text,
+)
 from .base import Base
 
 __all__ = [
+    # Types
+    "integer",
+    "real",
+    "boolean",
+    "text",
     # Mixins
     "IDMixin",
     "TimeStampMixin",
